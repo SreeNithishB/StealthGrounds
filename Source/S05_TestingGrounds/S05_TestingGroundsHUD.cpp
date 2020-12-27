9e9e8048abf41b60363c8ae076cfc946a71f514e
Sree Nithish B <sreenithishbofficial@gmail.com>
2020-12-27 17:31:32 +0530

    Updated GameMode & HUD after refractoring

--- Source/S05_TestingGrounds/S05_TestingGroundsHUD.cpp
+++ Source/S05_TestingGrounds/S05_TestingGroundsHUD.cpp
@@ -10,7 +10,7 @@
 AS05_TestingGroundsHUD::AS05_TestingGroundsHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
