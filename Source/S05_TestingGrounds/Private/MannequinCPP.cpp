894d223c27347602bd889378137691a6694bdd1d
Sree Nithish B <sreenithishbofficial@gmail.com>
2020-12-30 20:25:47 +0530

    Converting Character BP into c++

--- Source/S05_TestingGrounds/Private/MannequinCPP.cpp
+++ Source/S05_TestingGrounds/Private/MannequinCPP.cpp
@@ -2,6 +2,9 @@
 
 
 #include "MannequinCPP.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Gun.h"
 
 // Sets default values
 AMannequinCPP::AMannequinCPP()
@@ -9,12 +12,37 @@ AMannequinCPP::AMannequinCPP()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CameraFP = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	CameraFP->SetupAttachment(GetCapsuleComponent());
+	CameraFP->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f)); // Position the camera
+	CameraFP->bUsePawnControlRotation = true;
+
+	FPArms = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	FPArms->SetOnlyOwnerSee(true);
+	FPArms->SetupAttachment(CameraFP);
+	FPArms->bCastDynamicShadow = false;
+	FPArms->CastShadow = false;
+	FPArms->SetRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
+	FPArms->SetRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
+
 }
 
 // Called when the game starts or when spawned
 void AMannequinCPP::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (GunBlueprint == NULL)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("GunBlueprint is NULL"));
+		return;
+	}
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	Gun->AttachToComponent(FPArms, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AnimInstance = FPArms->GetAnimInstance();
+
+	// Bind fire event
+	//InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
 	
 }
 
@@ -30,5 +58,10 @@ void AMannequinCPP::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void AMannequinCPP::Fire()
+{
+	Gun->OnFire();
 }
 
--- Source/S05_TestingGrounds/Public/MannequinCPP.h
+++ Source/S05_TestingGrounds/Public/MannequinCPP.h
@@ -6,10 +6,20 @@
 #include "GameFramework/Character.h"
 #include "MannequinCPP.generated.h"
 
+class UCameraComponent;
+class USkeletalMeshComponent;
+class AGun;
+
 UCLASS()
 class S05_TESTINGGROUNDS_API AMannequinCPP : public ACharacter
 {
 	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* CameraFP;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	USkeletalMeshComponent* FPArms;
 
 public:
 	// Sets default values for this character's properties
@@ -19,11 +29,21 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	AGun* Gun;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	TSubclassOf<AGun> GunBlueprint;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void Fire();
 
 };
