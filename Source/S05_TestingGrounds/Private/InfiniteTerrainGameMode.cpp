8a53a3d491ad7373357b82f758b92a49c05f9ca8
Sree Nithish B <sreenithishbofficial@gmail.com>
2021-01-01 21:52:51 +0530

    Find Actors of Type from c++

---
+++ Source/S05_TestingGrounds/Private/InfiniteTerrainGameMode.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InfiniteTerrainGameMode.h"
+#include "NavMesh/NavMeshBoundsVolume.h"
+#include "EngineUtils.h"
+
+void AInfiniteTerrainGameMode::PopulateBoundsVolumePool()
+{
+	TActorIterator<ANavMeshBoundsVolume> VolumeIterator = TActorIterator<ANavMeshBoundsVolume>(GetWorld());
+	while (VolumeIterator)
+	{
+		AddToPool(*VolumeIterator);
+		++VolumeIterator;
+	}
+}
+
+
+void AInfiniteTerrainGameMode::AddToPool(ANavMeshBoundsVolume* VolumeToAdd)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Found Actor: %s"), *VolumeToAdd->GetName());
+}
